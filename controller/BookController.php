a64401ed900b511bd3c86862d372835e0142516e
toufikdev <trensrealcontent@gmail.com>
2022-09-07 14:59:21 +0100

    book crud done complete!

--- controller/BookController.php
+++ controller/BookController.php
@@ -21,20 +21,38 @@ class BookController{
     public static function create($req)
     {
         $book = new Book();
-        $author = AuthorController::show($req['author']);
-        $category = CategoryController::show($req['category']);
-        $book->add($book->construct_for_insert($req['title'],$author,$category,"",$req['publish']));
-        header("location: book_view.php");
+        $target_path = "../images/";
+        $file_name =  date("YmdHis-").trim(' ').basename($_FILES['image']['name']);
+        $target_path = $target_path .$file_name;
+        $image = $file_name;
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
+            sleep(2);
+            $author = AuthorController::show($req['author']);
+            $category = CategoryController::show($req['category']);
+            $book->add($book->construct_for_insert($req['title'],$author,$category,$image,$req['publish']));
+        } else {
+            header("location: book_view.php");
+        }
     }
-
 
     public static function update($req)
     {
+        
         $book = new Book();
-        $author = AuthorController::show($req['author']);
-        $category = CategoryController::show($req['category']);
-        $book->update($book->construct($req['id'],$req['title'],$author,$category,"",$req['publish']));
-        header("location: book_view.php");
+        unlink('../images/' . $book->find($req['id'])->get_image());
+        $target_path = "../images/";
+        $file_name =  date("YmdHis-").trim(' ').basename($_FILES['image']['name']);
+        $target_path = $target_path .$file_name;
+        $image = $file_name;
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
+            sleep(2);
+            $author = AuthorController::show($req['author']);
+            $category = CategoryController::show($req['category']);
+            $book->update($book->construct($req['id'],$req['title'],$author,$category,$image,$req['publish']));
+            header("location: book_view.php");
+        } else {
+            header("location: book_view.php");
+        }
     }
 
     public static function delete($id)
